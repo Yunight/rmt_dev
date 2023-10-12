548f97a75d84fa9f737fe89c3309290cf6358fda
Benchi <benchi.chen.bc@gmail.com>
2023-10-12 02:55:04 +0200

    adding webpack, babels etc..

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    plugins: [
+        require("autoprefixer"),
+        require("cssnano"),
+        require("postcss-pxtorem")({
+            propList: ['*']
+        }),
+    ]
+}
